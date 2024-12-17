b8a3fbd61de8b5b06664fb63dc1ad3557e0ff15b
devthejo <devthejo@protonmail.com>
2024-12-17 17:24:07 +0100

    fix: default loader

--- packages/core/src/components/Loader.jsx
+++ packages/core/src/components/Loader.jsx
@@ -1,5 +1,17 @@
-import { SpinnerInfinity } from "spinners-react"
+import { SpinnerInfinity } from "spinners-react/src/SpinnerInfinity"
 
 export default function Loader() {
-  return <SpinnerInfinity />
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        minHeight: "100vh",
+        width: "100%",
+      }}
+    >
+      <SpinnerInfinity size={100} thickness={100} speed={100} color="green" />
+    </div>
+  )
 }
